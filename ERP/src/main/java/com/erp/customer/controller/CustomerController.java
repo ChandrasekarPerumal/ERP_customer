93755601173ed2968df6d26d48045e11fed66834
ChandrasekarPerumal <chandruselv@gmail.com>
2024-01-14 00:33:51 +0530

    Base skeleton is added for CustomerCntlr and Entity class

--- ERP/src/main/java/com/erp/customer/controller/CustomerController.java
+++ ERP/src/main/java/com/erp/customer/controller/CustomerController.java
@@ -1,15 +1,52 @@
 package com.erp.customer.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("erp/api/v1/customers")
 public class CustomerController {
 
-	@GetMapping("/")
-	public String testing() {
-		return "Application is up";
+
+	// Get customer data by using customer-id
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getCustomerById() {
+		return null;
 	}
-	
-	
+
+	// Get all the customer
+	@GetMapping("/")
+	public List<?> getAll() {
+		return null;
+	}
+
+
+	// Store new customer data
+	@PostMapping("/new")
+	public ResponseEntity<String> saveNewCustomerData() {
+
+		return null;
+	}
+
+	// Update the customer data using customer-id
+	@PutMapping("/{id}")
+	public ResponseEntity<String> updateCustomerById() {
+		return null;
+	}
+
+	// Delete the customer data using customer-id
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteCustomerById(@PathVariable int id) {
+		return null;
+	}
+
 }
---
+++ ERP/src/main/java/com/erp/customer/entity/Customer.java
@@ -0,0 +1,130 @@
+package com.erp.customer.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "customer",schema = "erp")
+public class Customer {
+	
+//	customerId, name, email, phone, address, companyName, industryType,
+//	customerStatus, accountManager, and audit columns.
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@NotNull
+	@Column(name="name")
+	private String name;
+	
+	@Email
+	@Column(name = "email_id")
+	private String emailId;
+	
+	@Size(min = 10,max = 10,message = "Phone number should be 10 digits")
+	@Column(name="phone_number")
+	private String phoneNumber;
+	
+	@Column(name="address")
+	private String address;
+	
+	@Column(name="company_name")
+	private String companyName;
+	
+	@Column(name="industry_type")
+	private String industryType;
+	
+	@Column(name="customer_status")
+	private String customerStatus;
+	
+	@Column(name="account_manager")
+	private String accountManager;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getIndustryType() {
+		return industryType;
+	}
+
+	public void setIndustryType(String industryType) {
+		this.industryType = industryType;
+	}
+
+	public String getCustomerStatus() {
+		return customerStatus;
+	}
+
+	public void setCustomerStatus(String customerStatus) {
+		this.customerStatus = customerStatus;
+	}
+
+	public String getAccountManager() {
+		return accountManager;
+	}
+
+	public void setAccountManager(String accountManager) {
+		this.accountManager = accountManager;
+	}
+	
+	
+	
+	
+	
+
+}
